ae6d580839cc3160c0108079970d7336c7bdb8b1
algroths <74266014+algroths@users.noreply.github.com>
2022-08-03 05:02:07 +0200

    Add files via upload

--- calculo.js
+++ calculo.js
@@ -2,14 +2,12 @@ const calculo = () => {
     let num = process.argv[2]
     const obj = []
     if (num === "undefined") {
-
-        let num = 1 * 10e5
+        num = 1 * 10e5
     }
 
     for (let i = 0; i < num; i++) {
         let random = Math.round(Math.random() * 1000)
 
-        console.log(random)
         obj.push(random)
     }
 
